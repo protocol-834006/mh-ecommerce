fb9e165a8eec87b24b87bb0d0143a264b9ad87ae
Prashant Kumar <prashant13cse075@gmail.com>
2018-11-27 15:31:28 +0530

    [master] Header component added

---
+++ packages/app/src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * Header component
+ * @type {React.PureComponent}
+ */
+class Header extends React.PureComponent {
+  render() {
+    return (
+      <div className="mhr-header">
+        <h1>This is a header component</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
